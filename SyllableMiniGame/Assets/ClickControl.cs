e2fdb0a0df38e43e92ae401084465cb3c1514298
Karel Haapasalo <karel.haapasalo@hotmail.com>
2020-04-03 14:54:35 +0300

    add ClickControl.cs
    For capturing string of a button text box when clicked

---
+++ SyllableMiniGame/Assets/ClickControl.cs
@@ -0,0 +1,28 @@
+﻿// Picks up values from components on click
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ClickControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Captures clicked syllable in a text field
+    void OnMouseDown()
+    {
+        Game1Manager.currentWord+=GetComponent<Text>().text;   
+        // This statement is for testing purposes
+        Debug.Log(Game1Manager.currentWord);     
+    }
+}
